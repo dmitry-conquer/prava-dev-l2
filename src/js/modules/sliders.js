83740c7775c136ed19fb7bdb973f78e771401c64
Dmitry Conquer <dmitry.conquer@gmail.com>
2024-03-05 13:22:40 +0200

    Коментарі

--- src/js/modules/sliders.js
+++ src/js/modules/sliders.js
@@ -1,4 +1,4 @@
-import Swiper, { Pagination } from "swiper";
+import Swiper, { Pagination, Navigation } from "swiper";
 
 const slider = document.querySelector(".slider");
 if (slider) {
@@ -13,6 +13,43 @@ if (slider) {
     pagination: {
        el: '.pagination',
        clickable: true,
+    },
+
+    breakpoints: {
+      320: {
+        slidesPerView: 1.02,
+      },
+      460: {
+         slidesPerView: 1.3,
+      },
+      640: {
+         slidesPerView: 2.1,
+      },
+      1024: {
+         slidesPerView: 4,
+      },
+    },
+  });
+}
+
+
+const commentsSlider = document.querySelector(".comments-slider");
+if (commentsSlider) {
+  new Swiper(commentsSlider, {
+    modules: [Pagination, Navigation],
+    wrapperClass: "comments-slider-wrapper",
+    slideClass: "comments-slider-slide",
+    slidesPerView: 4,
+    spaceBetween: 11,
+    speed: 1200,
+
+    pagination: {
+       el: '.pagination-comments',
+       clickable: true,
+    },
+    navigation: {
+      prevEl: '.comments-prev',
+      nextEl: '.comments-next',
     },
 
     breakpoints: {
